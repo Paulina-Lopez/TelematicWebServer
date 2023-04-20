e76cd3a2261155da0275b51dbfe055c22dbe4d24
Paulina-Lopez <lopezpaulina22@gmail.com>
2023-04-20 17:54:39 -0500

    Despliegue

--- servidorweb.py
+++ servidorweb.py
@@ -6,7 +6,7 @@ port =  8080
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.setsockopt(socket.SOL_SOCKET , socket.SO_REUSEADDR , 1)
-serversocket.bind((host , port))
+serversocket.connect((host , port))
 serversocket.listen(1)
 print('servidor en el puerto',port)
 
